701a797f7921a6270213a458a7b53dd59134b0a8
selametle <selametle@mail.com>
2018-07-30 19:37:21 -0500

    OfficeList class added

---
+++ src/main/java/com/inss/atmn/OfficeList.java
@@ -0,0 +1,87 @@
+package com.inss.atmn;
+
+public class OfficeList {
+
+  private String state;
+  private int district;
+  private String firstName;
+  private String lastName;
+  private String party;
+  private String officeRoom;
+  private String phone;
+  private String email;
+  private String committeeAssignment;
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public int getDistrict() {
+    return district;
+  }
+
+  public void setDistrict(int district) {
+    this.district = district;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getParty() {
+    return party;
+  }
+
+  public void setParty(String party) {
+    this.party = party;
+  }
+
+  public String getOfficeRoom() {
+    return officeRoom;
+  }
+
+  public void setOfficeRoom(String officeRoom) {
+    this.officeRoom = officeRoom;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getCommitteeAssignment() {
+    return committeeAssignment;
+  }
+
+  public void setCommitteeAssignment(String committeeAssignment) {
+    this.committeeAssignment = committeeAssignment;
+  }
+
+}
